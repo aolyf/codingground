1e09d89b9c286cbf4624c6d3f85785d0a411d811
calico <ccintj@gmail.com>
2016-08-22 16:20:40 -0400

    Created by Coding Ground Client

---
+++ New Project-20160822/main.py
@@ -0,0 +1,25 @@
+#import math
+'''#VARIABLES
+a = 1 #int
+b = 2.0 #float
+c = "a" #string
+d = True #bool
+
+print a
+print b 
+print c
+print d
+'''
+'''
+#Arithemetic Operators
+print (2 + 2)
+print (2 * 2)
+print (2 / 2)
+print (2 // 2)
+print (2 ** 2)
+print (2 % 1)
+'''
+#Modules and Functions
+print math.floor(2.3)
+print math.ceil(2.3)
+
